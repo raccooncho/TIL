702dec57cbbe787bfc49831bd662fe4b7dd1df4e
raccooncho <wjtjdrb@naver.com>
2019-03-08 16:55:57 +0900

    ??;

--- 08_ALGORITHM/test.py
+++ 08_ALGORITHM/test.py
@@ -1,13 +1,32 @@
-import sys
-sys.stdin = open('input1.txt', 'r')
+for t in range(10):
+    a = input()
+    miro = []
+    for i in range(16):
+        ass = input()
+        row = [x for x in ass]
+        miro.append(row)
+    former_status = []
+    now_status = ['11']
+    finds = ['2']
+    while '3' not in finds or now_status == []:
+        former_status_dum = now_status[:]
+        now_status_dum = []
+        finds_dum = []
+        for pos in range(len(now_status)):
+            aa = int(now_status[pos][0])
+            b = int(now_status[pos][1])
+            check = [str(aa-1)+str(b-2), str(aa-1)+str(b+1), str(a+1)+ str(b-1), str(a+1)+str(b+1)]
+            for direc in check:
+                aaa = int(direc[0])
+                bb = int(direc[1])
+                mm = miro[aaa][bb]
+                if direc not in former_status and mm != 1:
+                    now_status_dum.append(direc)
+                    finds_dum.append(mm)
+        now_status = now_status.dum
+        finds = finds_dum
+    if now_status:
+        print(f'#{a} 1')
+    else:
+        print(f'#{a} 0')
 
-
-for testcase in range(10):
-    input()
-    case = list(map(int, input().split()))
-    answer = 0
-    for i in range(2, len(case)-2):
-        high = max(case[i-1], case[i-2], case[i+1], case[i+2])
-        if case[i] > high:
-            answer += case[i] - high
-    print(f'#{testcase+1} {answer}')
--- 999_JUNKFILE/test.py
+++ 999_JUNKFILE/test.py
@@ -1,19 +1,39 @@
-import sys
-sys.stdin = open('input (6).txt', 'r')
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Model
 
-number = int(input())
-for i in range(number):
-    problem = input()
-    problem = problem.lstrip('0')
-    if not len(problem):
-        print(f'#{i+1} 0')
-    else:
-        answer = problem[0]
-        if len(problem) > 1:
-            for s in range(1, len(problem)):
-                if problem[s] != problem[s-1]:
-                    answer += problem[s]
-            result = len(answer)
-            print(f'#{i+1} {result}')
-        else:
-            print(f'#{i+1} 1')
+def list(request):
+    my_models = Model.objects.all()
+    context = {'my_models': my_models}
+    return render(request, my_model/list.html, context)
+
+def detail(request, id):
+    my_model = get_object_or_404(Model, id=id)
+    return render(request, my_model/detail.html, {'my_model': my_model})
+
+def delete(request, id):
+    if request.method == "POST":
+        my_model = Model.objects.get(id=id)
+        my_model.delete()
+    return redirect('my_model:list')
+
+
+-------------------------------------------------------------------
+from django.urls import path
+from . import views
+
+app_name = 'my_model'
+
+urlpatterns=[
+    path('', views.list, name='list'),
+    path('<int:id>/', views.detail, name='detail'),
+    path('<int:id>/delete/', views.delete, name='delete'),
+]
+
+-------------------------------------------------------------------
+
+from djange.db import models
+
+class my_model(models.Model):
+    ...and
+
+-------------------------------------------------------------------
